8350c2bc90a9c7fafc78c65384ee550b55c27fb6
zhelyazkovse <zhelyazkovse@gmail.com>
2024-04-11 14:51:12 +0200

    search/fetch/index adjustments

--- src/data/fetch-gifs.js
+++ src/data/fetch-gifs.js
@@ -6,3 +6,10 @@ export const fetchTrendingGifs = (limit) => {
     .then((data) => data.data)
     .catch((error) => console.error("Error fetching trending gifs:", error));
 };
+
+export function fetchSearchGifs(query) {
+  fetch(`${API_URL}/search?api_key=${API_KEY}&q=${query}&limit=24`)
+    .then(response => response.json())
+    .then(data => data.data)
+    .catch(error => console.error("Error fetching search results:", error));
+}
--- src/events/search-events.js
+++ src/events/search-events.js
@@ -1,11 +1,10 @@
 import { CONTAINER_SELECTOR } from "../common/constants.js";
-// import { loadSearchMovies } from "../requests/request-service.js";
 import { toSearchView } from "../views/search-view.js";
 import { q } from "./helpers.js";
-
+import { fetchSearchGifs } from "../data/fetch-gifs.js";
 export const renderSearchItems = (searchTerm) => {
-  const movies = loadSearchMovies(searchTerm);
-
-  q(CONTAINER_SELECTOR).innerHTML = toSearchView(movies, searchTerm);
+    fetchSearchGifs(searchTerm).then(() => {
+        q(CONTAINER_SELECTOR).innerHTML = toSearchView(searchTerm);
+    });
 };
 // search bar
