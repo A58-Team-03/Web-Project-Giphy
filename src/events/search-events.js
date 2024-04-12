693091212369f6f435ccb72693925b3659981649
zhelyazkovse <zhelyazkovse@gmail.com>
2024-04-12 18:23:45 +0200

    update index/search-views

--- src/events/search-events.js
+++ src/events/search-events.js
@@ -1,12 +1,12 @@
 import { CONTAINER_SELECTOR } from "../common/constants.js";
 import { fetchSearchGifs } from "../data/fetch-gifs.js";
-import { toSearchView } from "../views/search-view.js";
-import { q } from "./helpers.js";
 import { displayGifs } from "../views/gif-display.js";
-
+import { q } from "./helpers.js";  
 
 export const renderSearchItems = (searchTerm) => {
   fetchSearchGifs(searchTerm).then(gifs => {
-    displayGifs(gifs, CONTAINER_SELECTOR);
-  }).catch(error => console.error("Failed to fetch and display search results:", error));
+    displayGifs(gifs, q(CONTAINER_SELECTOR));  
+  }).catch(error => {
+    console.error("Failed to fetch and display search results:", error);
+  });
 };
--- src/index.js
+++ src/index.js
@@ -1,54 +1,94 @@
 import { HOME } from './common/constants.js';
-import { toggleFavoriteStatus } from './events/favorites-events.js';
-import { q } from './events/helpers.js';
-import { loadPage, renderCategory, renderMovieDetails } from './events/navigation-events.js';
+import { loadPage, renderGifDetails } from './events/navigation-events.js';
 import { renderSearchItems } from './events/search-events.js';
+import { q } from './events/helpers.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-
-  // add global listener
-  document.addEventListener('click', event => {
-
-    // nav events
-    if (event.target.classList.contains('nav-link')) {
-
-      loadPage(event.target.getAttribute('data-page'));
-    }
-
-    // show category events
-    if (event.target.classList.contains('view-category-btn')) {
-      renderCategory(+event.target.getAttribute('data-category-id'));
-    }
-
-    // show movie events
-    if (event.target.classList.contains('view-movie-btn')) {
-      renderMovieDetails(+event.target.getAttribute('data-movie-id'));
-    }
-
-    // toggle favorite event
-    if (event.target.classList.contains('favorite')) {
-      toggleFavoriteStatus(+event.target.getAttribute('data-movie-id'));
-    }
-
-  });
-
-  // // search events
-  // q('input#search').addEventListener('input', e => {
-  //   renderSearchItems(e.target.value);
-  // });
-
-  q("button#search-button").addEventListener("click", () => {
-    const searchTerm = q("input#search").value;
-    renderSearchItems(searchTerm);
-  });
-
   loadPage(HOME);
 
+  const searchButton = q("#search-button");
+  const searchInput = q("#search-input");
+  const logo = q(".team-name");
+
+  searchButton.addEventListener("click", () => {
+    renderSearchItems(searchInput.value);
+  });
+
+  searchInput.addEventListener("keypress", event => {
+    if (event.key === "Enter") {
+      renderSearchItems(searchInput.value);
+    }
+  });
+
+  logo.addEventListener("click", () => {
+    loadPage(HOME);
+  });
+
+  document.getElementById("trending-gifs").addEventListener("click", event => {
+    if (event.target.tagName === 'IMG') {
+      const gifId = event.target.dataset.id;  
+      renderGifDetails(gifId);
+    }
+  });
 });
 
-q("input#search").addEventListener("keypress", (event) => {
-  if (event.key === "Enter") {
-    const searchTerm = q("input#search").value;
-    renderSearchItems(searchTerm);
-  }
-});
+
+
+
+
+// import { HOME } from './common/constants.js';
+// import { toggleFavoriteStatus } from './events/favorites-events.js';
+// import { q } from './events/helpers.js';
+// import { loadPage, renderCategory, renderMovieDetails } from './events/navigation-events.js';
+// import { renderSearchItems } from './events/search-events.js';
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+
+//   // add global listener
+//   document.addEventListener('click', event => {
+
+//     // nav events
+//     if (event.target.classList.contains('nav-link')) {
+
+//       loadPage(event.target.getAttribute('data-page'));
+//     }
+
+//     // show category events
+//     if (event.target.classList.contains('view-category-btn')) {
+//       renderCategory(+event.target.getAttribute('data-category-id'));
+//     }
+
+//     // show movie events
+//     if (event.target.classList.contains('view-movie-btn')) {
+//       renderMovieDetails(+event.target.getAttribute('data-movie-id'));
+//     }
+
+//     // toggle favorite event
+//     if (event.target.classList.contains('favorite')) {
+//       toggleFavoriteStatus(+event.target.getAttribute('data-movie-id'));
+//     }
+
+//   });
+
+//   // // search events
+//   // q('input#search').addEventListener('input', e => {
+//   //   renderSearchItems(e.target.value);
+//   // });
+
+//   q("button#search-button").addEventListener("click", () => {
+//     const searchTerm = q("input#search").value;
+//     renderSearchItems(searchTerm);
+//   });
+
+//   loadPage(HOME);
+
+// });
+
+// q("input#search").addEventListener("keypress", (event) => {
+//   if (event.key === "Enter") {
+//     const searchTerm = q("input#search").value;
+//     renderSearchItems(searchTerm);
+//   }
+// });
