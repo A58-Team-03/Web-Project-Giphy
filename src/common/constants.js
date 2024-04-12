001afae96fced4bdd2d6462ca44ed0ba8630ad8c
KaloyanKostov <kostov.kaloyan44@gmail.com>
2024-04-12 22:33:43 +0300

    Refactor constants

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,15 +1,13 @@
-export const API_KEY = "A4NbxBggRgZdNEfkfCR6Qsg1jyZIJ9or"; 
+export const API_KEY = "yXA00USmfaHrPXxUyzTAyW7CZW7Sqqkw";
 export const API_URL = "https://api.giphy.com/v1/gifs";
-export const CONTAINER_SELECTOR = '#trending-gifs';
-export const FAVORITES = 'favorites';
-export const FULL_HEART = '❤';
-export const EMPTY_HEART = '♡';
-export const HOME = 'home'
+export const API_UPLOAD_URL = "https://upload.giphy.com/v1/gifs";
+export const CONTAINER_SELECTOR = "#trending-gifs";
+export const FAVORITES = "favorites";
+export const FULL_HEART = "❤";
+export const EMPTY_HEART = "♡";
+export const HOME = "home";
+export const UPLOAD = "upload";
 export const LIMIT_GIFS = 25;
-
-
-
-
 
 // export const HOME = "home";
 
@@ -27,4 +25,3 @@ export const LIMIT_GIFS = 25;
 
 // export const API_KEY = "yXA00USmfaHrPXxUyzTAyW7CZW7Sqqkw";
 // export const API_URL = "https://api.giphy.com/v1/gifs";
-
