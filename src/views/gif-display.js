267cd43f09885021b84dc4e4604d55ca864ebfcf
JordanDim <dankooo@gmail.com>
2024-04-14 06:12:32 +0300

    add gif.id in displayGifs for details-view

--- src/views/gif-display.js
+++ src/views/gif-display.js
@@ -1,11 +1,13 @@
-
-
 export const displayGifs = (gifs, containerSelector) => {
-    const container = document.querySelector(containerSelector);
-    container.innerHTML = ''; 
-    gifs.forEach(gif => {
-      const img = document.createElement('img');
-      img.src = gif.images.original.url;
-      container.appendChild(img);
-    });
-  };
+  const container = document.querySelector(containerSelector);
+  container.innerHTML = ''; 
+  gifs.forEach(gif => {
+    const gifContainer = document.createElement('div');
+    gifContainer.classList.add('gif-data');
+    gifContainer.id = gif.id; 
+    const img = document.createElement('img');
+    img.src = gif.images.original.url;
+    gifContainer.appendChild(img);
+    container.appendChild(gifContainer);
+  });
+};
