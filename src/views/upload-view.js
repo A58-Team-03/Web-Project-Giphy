9db7419edc0cb0d3b35cb70cde816822fd61a41f
KaloyanKostov <kostov.kaloyan44@gmail.com>
2024-04-15 11:29:52 +0300

    Refactor upload-view

--- src/views/upload-view.js
+++ src/views/upload-view.js
@@ -1,7 +1,6 @@
 export const toUploadView = () => `
-<form id="uploadForm" class="upload">
-    <h1>Upload here</h1>
-    <input type="file" id="fileInput" accept=".gif">
-    <button type="submit" class="upload">Upload</button>
+<form id="upload-form" class="upload">
+  <input type="file" id="fileInput" accept="image/gif" ">
+  <button type="submit" class="upload">Upload</button>
 </form>
 `;
