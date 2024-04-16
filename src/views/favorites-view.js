abc00a7d7a201840bb2f514203d25c1fec42f710
JordanDim <dankooo@gmail.com>
2024-04-16 17:21:11 +0300

    implemented random gif

--- src/views/favorites-view.js
+++ src/views/favorites-view.js
@@ -1,10 +1,13 @@
+import { fetchRandomGif } from '../data/fetch-gifs.js';
+import { displayGifs } from './gif-display.js';
 import { toMovieSimple } from './movie-views.js';
 
-export const toFavoritesView = (gifs) => `
+
+export const toFavoritesView = async (gifs) => `
 <div id="gifs">
   <h1>Favorite gifs:</h1>
   <div class="content">
-    ${gifs || '<p>Favorite some gifs to see them here.</p>'}
+    ${gifs || displayGifs([await fetchRandomGif()])};
   </div>
 </div>
 `;
