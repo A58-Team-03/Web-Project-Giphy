8265ddb7a2e62d32dcb76e67b10d0462489f145c
JordanDim <dankooo@gmail.com>
2024-04-16 18:47:15 +0300

    fix random fav

--- src/views/favorites-view.js
+++ src/views/favorites-view.js
@@ -3,11 +3,19 @@ import { displayGifs } from './gif-display.js';
 import { toMovieSimple } from './movie-views.js';
 
 
+/**
+ * Converts an array of gifs into a favorites view HTML string.
+ * @param {Array} gifs - The array of gifs to be displayed.
+ * @returns {string} - The HTML string representing the favorites view.
+ */
 export const toFavoritesView = async (gifs) => `
 <div id="gifs">
   <h1>Favorite gifs:</h1>
   <div class="content">
     ${gifs || displayGifs([await fetchRandomGif()])};
+    
+    ${(displayGifs([await fetchRandomGif()]))};
   </div>
 </div>
 `;
+// ${console.log(await fetchRandomGif())};
