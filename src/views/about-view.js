26029a02282f1dae261b1d42f934ccbab76e29c7
JordanDim <dankooo@gmail.com>
2024-04-16 13:15:02 +0300

    doc for about page

--- src/views/about-view.js
+++ src/views/about-view.js
@@ -1,9 +1,12 @@
+/** Generates HTML for About page
+ * @return {string} HTML for About page
+ */
 export const toAboutView = () => `
 <div id="about">
   <div class="content">
     <h1>About the app</h1>
-    <h2>Authors: Telerik Academy</h2>
-    <h2>Date: 2021</h2>
+    <h2>Authors: Team 03</h2>
+    <h2>Date: April 2024</h2>
   </div>
 </div>
 `;
